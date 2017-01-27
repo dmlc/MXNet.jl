48924ef939b165d54e754a003a5d124fc35ba92e
Valentin Churavy <v.churavy@gmail.com>
2017-01-27 15:40:16 +0900

    update libmxnet tag and windows download

--- deps/build.jl
+++ deps/build.jl
@@ -5,8 +5,8 @@ import JSON
 # First try to detect and load existing libmxnet
 ################################################################################
 libmxnet_detected = false
-libmxnet_curr_ver = "873b92856d0c42198716e4df206d9387d0d59ec8" # HEAD of 2016.09.08
-curr_win = "20161125"
+libmxnet_curr_ver = "v0.9.3"
+curr_win = "20170122"
 
 if haskey(ENV, "MXNET_HOME")
   info("MXNET_HOME environment detected: $(ENV["MXNET_HOME"])")
